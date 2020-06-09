2df0e625b980e07ca25e65510df756b0aca66fd3
Prateek Pandey <46824331+pprockys@users.noreply.github.com>
2020-06-09 14:53:13 +0530

    Create Day_09_Is_Subsequence.cpp

---
+++ solutions/Day_09_Is_Subsequence.cpp
@@ -0,0 +1,15 @@
+//pprockys;
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        while(j<t.length()&&i<s.length())
+        {
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+        return i==s.length();
+    }
+};
